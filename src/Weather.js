12d9ec7678f3261520862639a1843224789f53c8
kevthedevv <kevin.esposado@gmail.com>
2020-12-11 22:34:08 +0800

    12112020

---
+++ src/Weather.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function Weather() {
+    return (
+        <div>
+            <main>
+                <div className="header">
+
+                </div>
+                <div className="container">
+                    <div className="content">
+                        <div>
+                            <h1 id="city"></h1>
+                            <p id="weather-type"></p>
+                        </div>
+                        <div>
+                            weather-icon
+                        </div>
+                        <div id="humidity-div">
+                            <img></img>
+                            <p>Humidity</p>
+                            <h1>%</h1>
+                        </div>
+                        <div id="temp-div">
+                            <img></img>
+                            <p>Temperature</p>
+                            <h1>'C</h1>
+                        </div>
+                        <div id="chance-div">
+                            <img></img>
+                            <p>Chances of rain</p>
+                            <h1>%</h1>
+                        </div>
+                        <div id="windspeed-div">
+                            <img></img>
+                            <p>Wind speed</p>
+                            <h1>kph</h1>
+                        </div>
+                    </div>
+                    <footer>Copyright: Vin Espoasdo 2020</footer>
+
+                </div>
+                
+            </main>
+        </div>
+    )
+}
