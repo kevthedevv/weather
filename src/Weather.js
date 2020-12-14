2f3d37a058eb7f8452c4298783ce9c038fe39808
ke185031 <ke185031@WPHKE185031-9GF>
2020-12-14 15:10:54 +0800

    121420

--- src/Weather.js
+++ src/Weather.js
@@ -1,79 +1,113 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import './Weather.css';
+import FormControl from '@material-ui/core'
 
-export default function Weather() {
-
+function Weather() {
 
     const key = 'API_KEY';
-
-
+    const [weatherType, setWeatherType] = useState("");
+    const [weatherDescription, setWeatherDescription] = useState("");
+    const [icon, setIcon] = useState("");
+    const [temp, setTemp] = useState("");
+    const [humidity, setHumidity] = useState("");
+    const [wind, setWind] = useState("");
+    const [city, setCity] = useState("");
+    const [searchCity, setSearchCity] = useState("manila")
+    const onChangeHandler = e =>{
+        e.preventDefault();
+        let cityvalue = e.target.value;
+        console.log(searchCity);
+    const transfer
+    }
 
 
     useEffect(() => {
-        fetch()
-      
-    }, [])
+         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&appid=84af6f866c5e0390408b69396b1f15f8`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            setWeatherType(data.weather[0].main);
+            setWeatherDescription(data.weather[0].description)
+            setIcon(data.weather[0].icon)
+            setTemp(data.main.temp);
+            setHumidity(data.main.humidity);
+            setWind(data.wind.speed);
+            setCity(data.name);
+        })
+    }, [searchCity])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
     return (
-        <div>
-            <main>
-                <div className="header">
-                    <div><h1><span>Just </span>Weather</h1></div>
-                    <div>
-                        <input type="text"></input>
-                        <div>burger</div>
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="content">
-                        <div>
-                            <h1 id="city"></h1>
-                            <p id="weather-type"></p>
-                        </div>
-                        <div>
-                            {/* we need to add icon here */}
-                            <img></img>
-                            <img></img>
-                        </div>
-                        <div id="humidity-div">
-                            <img></img>
-                            <p>Humidity</p>
-                            <h1>%</h1>
-                        </div>
-                        <div id="temp-div">
-                            <img></img>
-                            <p>Temperature</p>
-                            <h1>'C</h1>
-                        </div>
-                        <div id="chance-div">
-                            <img></img>
-                            <p>Chances of rain</p>
-                            <h1>%</h1>
-                        </div>
-                        <div id="windspeed-div">
-                            <img></img>
-                            <p>Wind speed</p>
-                            <h1>kph</h1>
-                        </div>
-                    </div>
-                    <footer>Copyright: Vin Espoasdo 2020</footer>
 
+        <div>
+            <main className="main-content" >
+                <div className="overlay">
+                    <div className="header">
+                        <div><h1><span id="just">Just </span>Weather</h1></div>
+                        <div id="search-div">
+                            <form>
+                                {/* <input type="text" placeholder="Search a city" onSubmit={(e) => onChangeHandler(e)}/> */}
+                                <input type="text" placeholder="Search a city" onChange={onChangeHandler} onSubmit={transfer}/>
+                            </form>
+                            <div>
+                                <img src="/weather-icons/menu-icon.png" alt="burger"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container">
+                        <div className="content">
+                            <div id="main-weather">
+                                <div id="city-div">
+                                    <h1 id="city">{city}</h1>
+                                    <p id="weather-type">{weatherType} - {weatherDescription}</p>
+                                </div>
+                                <div>
+                                {/* we need to add icon here */}
+                                <img src={"http://openweathermap.org/img/wn/" + icon + "@2x.png"} alt="alternatetext"></img>
+                                </div>
+                            </div>
+
+                            <div id="humidity-div" className="icons-main">
+
+                                <img src="/weather-icons/humid-icon.png"></img>
+                                <div className="icons-description">
+                                    <p>Humidity</p>
+                                    <h1>{humidity}%</h1>
+                                </div>
+
+                            </div>
+                            <div id="temp-div" className="icons-main">
+                                <img src="/weather-icons/temp-icon.png"></img>
+                                <div className="icons-description">
+                                    <p>Temperature</p>
+                                    <h1>{temp}'C</h1>
+                                </div>
+                            </div>
+                            <div id="chance-div" className="icons-main">
+                                <img src="/weather-icons/umbrella-icon.png"></img>
+                                <div className="icons-description">
+                                    <p>Chances of rain</p>
+                                    <h1>%</h1>
+                                </div>
+                            </div>
+                            <div id="windspeed-div" className="icons-main">
+                            <img src="/weather-icons/wind-icon.png"></img>
+                                <div>
+                                    <p>Wind speed</p>
+                                    <h1>{wind}kph</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="footer">
+                            <div>
+                                Copyright: Vin Espoasdo 2020
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                
             </main>
         </div>
     )
 }
+export default Weather;
